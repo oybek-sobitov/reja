cda0995305d7b3b4a16eed0cb7a22774b8fbf1e8
oybek-sobitov <oybeksobitovxyz@gmail.com>
2025-09-18 00:06:20 +0900

    feat: modified server and added new views file

--- server.js
+++ server.js
@@ -21,13 +21,15 @@ app.set("view engine", "ejs");
 
 // Section - 4 (Routing)
 
-app.get("/hello", function(req, res) {
-    res.end("<h1>Hello world by Oybek</h1>");
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"});
 });
 
-app.get("/gift", function(req, res) {
-    res.end("<h1>Siz sovg'alar sahifasidasiz!</h1>");
+app.get("/", function(req, res) {
+    res.render('harid');
 });
+
 
 const server = http.createServer(app);
 let PORT = 3000;
