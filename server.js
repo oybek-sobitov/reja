1f87fded549121ec880dc3bde7cdd8a3e056077b
oybek-sobitov <oybeksobitovxyz@gmail.com>
2025-09-18 22:40:55 +0900

    feat:developed author.ejs router file

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ console.log("Web serverni boshlash");
 
 const express = require('express');
 const http = require('http');
+const res = require('express/lib/response');
 const app = express();  // app --> Object
+const fs = require('fs');
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+})
+
 
 // Section - 1 (Enterence)
 
@@ -25,6 +37,10 @@ app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({test: "success"});
 });
+
+app.get('/author', (req, res) => {
+    res.render('author', { user: user });
+})
 
 app.get("/", function(req, res) {
     res.render('harid');
