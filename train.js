bdcbce9723c7504fa2164eb329b9e626c86a67c2
oybek-sobitov <oybeksobitovxyz@gmail.com>
2025-09-23 18:22:41 +0900

    feat: train.js final

--- train.js
+++ train.js
@@ -1,4 +1,4 @@
-const { links } = require("express/lib/response");
+/* const { links } = require("express/lib/response");
 
 console.log("Jack Ma maslahatlari");
 const list = [
@@ -8,9 +8,9 @@ const list = [
     "sizda kuchli bo'lgan narsalarni qiling", // 40-50
     "yoshlarga investitsiya qiling", // 50-60
     "endi dam oling, foydasi yo'q endi", // 60
-];
+]; */
 
-function maslahatBering (a, callback) {
+/* function maslahatBering (a, callback) {
     if(typeof a !== 'number') callback("insert a number", null);
     else if (a <=20) callback(null, list[0]);
     else if (a < 20 && a <= 30) callback(null, list[1]);
@@ -32,4 +32,73 @@ maslahatBering(65, (err, data) => {
     console.log('Javob:', data);
 })
 
-console.log("passed here 1")
+console.log("passed here 1") */
+
+// async function
+/* async function maslahatBering (a) {
+    if(typeof a !== 'number') throw new Error("insert a number");
+    else if (a <=20) return list[0];
+    else if (a < 20 && a <= 30) return list[1];
+    else if (a < 30 && a <= 40) return list[2];
+    else if (a < 40 && a <= 50) return list[3];
+    else if (a < 50 && a <= 60) return list[4];
+    else {
+        return new Promise((resolve, rejcet) => {
+            setTimeout(() => {
+                resolve(list[5]);
+            }, 5000)
+        });
+        
+        /*    setTimeout(function(){
+                return list[5];
+            }, 5000); */
+/*    }
+    
+} */ 
+ 
+
+// call via then / catch
+/* console.log("passed here 0")
+
+maslahatBering(25).then(data => {
+    console.log("Javob:", data);
+}).catch(err => {
+    console.log("ERROR:", err);
+});
+
+console.log("passed here 1") */
+
+// async / await
+/* async function run() {
+    let javob = await maslahatBering(20);
+    console.log(javob);
+    javob = await maslahatBering(35);
+    console.log(javob);
+    javob = await maslahatBering(45);
+    console.log(javob);
+}   
+
+run();
+
+*/
+
+
+// TASK A
+
+
+function countLetter(harf, soz) {
+  if (harf.length !== 1) {
+    return "Birinchi argumetga harf kiriting!";
+  }
+
+  let count = 0;
+  for (let a of soz) {
+    if (a === harf) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countLetter("r", "rear"));
