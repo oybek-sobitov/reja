18e90c80f21d37d7abafe8ac4d9d76552855f7f4
oybek-sobitov <oybeksobitovxyz@gmail.com>
2025-10-01 23:20:55 +0900

    feat: Added edit function and final version of project

--- public/browser.js
+++ public/browser.js
@@ -59,6 +59,28 @@ document.addEventListener("click", function(e)   {
 
 
     if(e.target.classList.contains("edit-me")) {
-        alert("Siz edit tugmasini bosdingiz");
+        let userInput = prompt("O'zgartirish kitiring", 
+        e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
+       if(userInput){
+            axios.post("/edit-item", 
+            {id: e.target.getAttribute("data-id"), 
+            new_input: userInput})
+            .then((response) => {
+                console.log(response.data);
+                e.target.parentElement.parentElement.querySelector(".item-text")
+                .innerHTML = userInput;
+            })   
+            .catch((err) => {
+                console.log("Iltimos qaytadan harakat qiling!"); 
+            })
+        }
     }
 });
+
+
+document.getElementById("clean-all").addEventListener("click", function(){
+    axios.post("/delete-all", {delete_all: true}).then(response => {
+        alert(response.data.state);
+        document.location.reload();
+    })
+})
